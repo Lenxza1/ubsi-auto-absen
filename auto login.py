81e50a61948dd7906e7da9bc6dec4f7aeb9fd101
Lenxza1 <naufal.rara55@gmail.com>
2024-01-04 17:18:18 +0700

    Update absen logic dan menambahkan log

--- auto login.py
+++ auto login.py
@@ -9,31 +9,27 @@ import locale
 import datetime
 import time
 
-mapelDict = {}
-mapelPerDayList = []
-TimeList = [] 
+mapelDetail = {} 
 isAbsenTime = bool
-sameDayMapel = 0
 totalAbsenHariIni = 0
 
 locale.setlocale(locale.LC_TIME, 'id_ID.UTF-8')
 current_date = datetime.datetime.now()
 formatted_time = current_date.strftime('%A - %H:%M')
-formatted_day = current_date.strftime("%A")
+today = current_date.strftime("%A")
 
 def absen():
     options = Options()
     options.page_load_strategy = "eager"
-    options.add_argument("--headless")
+    # options.add_argument('--headless=new')
 
-    driver = Chrome()
+    driver = Chrome(options= options)
     driver.get("http://elearning.bsi.ac.id")
     wait = WebDriverWait(driver, 10)
 
     isAbsenTime = False
 
-    TimeList = []
-    mapelPerDayList = []
+    mapelDetail = {}
     try:
         wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Masuk"))).click()
         wait.until(EC.presence_of_element_located((By.ID, "username"))).send_keys(user[1].nim)
@@ -50,40 +46,54 @@ def absen():
                 child_element = element.find_element(By.CSS_SELECTOR, ".pricing-save")
                 child_value = child_element.text
 
-                TimeList.append(child_value[8:])
-                mapelPerDayList.append(child_value[:-14])
                 sliced_value = child_value[:-6]
+                
+                if mapelDetail.get(child_value[:-14]) == None:
+                    mapelDetail[child_value[:-14]] = child_value[-11:]
+                else:
+                    temp = mapelDetail[child_value[:-14]]
+                    mapelDetail[child_value[:-14]] = [temp, child_value[-11:]]
 
-                if sliced_value == formatted_time:
+                if sliced_value == 'Kamis - 07:30':
                     element.find_element(By.CSS_SELECTOR, ".pricing-footer").find_element(By.LINK_TEXT, "Masuk Kelas").click()
                     isAbsenTime = True
             except StaleElementReferenceException:
                 break     
 
         if isAbsenTime == True:
-            wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[2]/div/div[5]/div/center/button"))).click()
-            driver.implicitly_wait(3)
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn.btn-primary.btn-rounded.left"))).click()
+            with open('logs.txt', 'a', newline='\n') as logs:
+                logs.write(f"{sliced_value}\n Sukses Absen \n")
         else:
-            print("Belum Saatnya untuk Absen")   
+            print("Belum Saatnya untuk Absen")
+            with open('logs.txt', 'a') as logs:
+                logs.write(f'{sliced_value}\n Belum saatnya untuk absen \n')
         
-        return mapelPerDayList, TimeList, isAbsenTime
+        return isAbsenTime, mapelDetail
     
     except Exception as e:
         print(f"Terjadi sebuah kesalahan: \n{e}")
+        with open('logs.txt', 'a') as logs:
+            logs.write(f'{sliced_value} \n {e} \n')
     finally:
         driver.quit()
 
 if __name__ == '__main__':
-    mapelPerDayList, TimeList, isAbsenTime = absen()
+    isAbsenTime, mapelDetail = absen()
 
-for i in range(len(mapelPerDayList)):
-    sameDayMapel = mapelPerDayList.count(mapelPerDayList[i])
-    mapelDict[mapelPerDayList[i]] = [sameDayMapel, TimeList]
-
-def parseTime(day, *args):
-    parsedArgs = args
-
-if formatted_day in mapelDict:
-    mapelCount = mapelDict[formatted_day]
+if today in mapelDetail:
+    for mapel, timeList in mapelDetail.items():
+        if today == mapel:
+            if isinstance(timeList, list):
+                mapelLenght = len(timeList)
+            else: 
+                timeDetails = []
+                timeDetails.append(timeList)
+                mapelLenght = len(timeDetails)
+    while totalAbsenHariIni < mapelLenght:
+        totalAbsenHariIni += 1
 else:
     time.sleep(86400)
+
+def SleepUntil(target: list):
+    pass
